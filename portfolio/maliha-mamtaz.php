a7fa3f2a677a66f7bfee4db24274871af4e4accd
maliha-mamtaz <maliha.mamtaz@northsouth.com>
2020-10-23 09:18:11 +0600

    portfolio

---
+++ portfolio/maliha-mamtaz.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>Portfolio</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<style>
+body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
+.w3-third img{margin-bottom: -6px; opacity: 0.8; cursor: pointer}
+.w3-third img:hover{opacity: 1}
+</style>
+<body class="w3-light-grey w3-content" style="max-width:1600px">
+
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-bar-block w3-white w3-animate-left w3-text-grey w3-collapse w3-top w3-center" style="z-index:3;width:300px;font-weight:bold" id="mySidebar"><br>
+  <h3 class="w3-padding-64 w3-center"><b>Maliha Mamtaz<br>Computer Engineer</b></h3>
+  <a href="#about" onclick="w3_close()" class="w3-bar-item w3-button">ABOUT ME</a> 
+  <a href="#contact" onclick="w3_close()" class="w3-bar-item w3-button">CONTACT</a>
+  <a href="javascript:void(0)" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-hide-large">CLOSE</a>
+</nav>
+
+<!-- Top menu on small screens -->
+<header class="w3-container w3-top w3-hide-large w3-white w3-xlarge w3-padding-16">
+  <span class="w3-left w3-padding"style="color:darkblue"><b>Maliha Mamtaz<br>Computer Engineer</b></span>
+  <a href="javascript:void(0)" class="w3-right w3-button w3-white" onclick="w3_open()">☰</a>
+</header>
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:300px">
+
+  <!-- Push down content on small screens --> 
+  <div class="w3-hide-large" style="margin-top:83px"></div>
+
+  <!-- About section -->
+  <div class="w3-container w3-dark-grey w3-center w3-text-light-grey w3-padding-32" id="about">
+    <h4><b>About Me</b></h4>
+    <img src="ami.jpg" alt="Paris"  width="400" height="500">
+
+    <div class="w3-content w3-justify" style="max-width:600px">
+      <p>I am studying Computer Scince and Engineering at North Soth University.
+      </p>
+
+       <hr class="w3-opacity">
+      <h4 class="w3-padding-16">Career Objective</h4>
+        <p>
+      </p> 
+     
+      <hr class="w3-opacity">
+      <h4 class="w3-padding-16">Technical Skills</h4>
+      <p class="w3-wide">Organizational Skills</p>
+      <div class="w3-white">
+        <div class="w3-container w3-padding-small w3-center w3-grey" style="width:95%">95%</div>
+      </div>
+      <p class="w3-wide">Web Design</p>
+      <div class="w3-white">
+        <div class="w3-container w3-padding-small w3-center w3-grey" style="width:90%">90%</div>
+      </div>
+      <p class="w3-wide">Web Development</p>
+      <div class="w3-white">
+        <div class="w3-container w3-padding-small w3-center w3-grey" style="width:90%">90%</div>
+      </div>
+       <p class="w3-wide">Cicuit Design</p>
+      <div class="w3-white">
+        <div class="w3-container w3-padding-small w3-center w3-grey" style="width:85%">85%</div>
+      </div>
+      <p class="w3-wide">Programming Language</p>
+      <div class="w3-white">
+        <div class="w3-container w3-padding-small w3-center w3-grey" style="width:80%">80%</div>
+      </div>
+       <p class="w3-wide">Database Design</p>
+      <div class="w3-white">
+        <div class="w3-container w3-padding-small w3-center w3-grey" style="width:100%">100%</div>
+      </div>
+      <p class="w3-wide">Apps Development</p>
+      <div class="w3-white">
+        <div class="w3-container w3-padding-small w3-center w3-grey" style="width:75%">75%</div>
+      </div>
+      
+      <hr class="w3-opacity">
+      <h4 class="w3-padding-16">Work Experience</h4>
+      <div class="w3-row-padding" style="margin:0 -16px">
+        <div class="w3-half w3-margin-bottom">
+          <ul class="w3-ul w3-white w3-center w3-opacity w3-hover-opacity-off">
+             <li class="w3-black w3-xlarge w3-padding-32"><b>2018<br>Entry Level Website Developer</b></li>
+             <li class="w3-black w3-xlarge w3-padding-32"><b>2019<br>Web Research</b></li>
+          </ul>
+        </div>
+      </div>
+
+      <hr class="w3-opacity">
+      <h4 class="w3-padding-16">Project Done</h4>
+      <div class="w3-row-padding" style="margin:0 -16px">
+        <div class="w3-half w3-margin-bottom">
+          <ul class="w3-ul w3-white w3-center w3-opacity w3-hover-opacity-off">
+            <i class="material-icons">git</i>
+             <span class="w3-tag w3-black w3-margin-bottom">https://github.com/maliha-mamtaz</span>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+      
+ 
+
+        <hr class="w3-opacity">
+      <h4 class="w3-padding-16">Interest</h4>
+      <div class="w3-content w3-justify" style="max-width:600px">
+        <p>
+          <span class="w3-tag w3-black w3-margin-bottom">Traveling</span>
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Critical Thinking</span> 
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Making Ideas</span>
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Knowing New People</span> 
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Communicating</span>
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Family</span>
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Sports</span> 
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Gadgets</span>
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Online Gaming</span> 
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">News</span> 
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Reading</span> 
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Physics</span>
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Clothing</span>
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Cooking</span>
+          <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Gardening</span>          
+        </p>
+      </div>
+
+  <!-- Footer -->
+  <footer class="w3-container w3-padding-32 w3-grey">  
+   
+
+       <!-- Contact section -->
+  <div class="w3-container w3-light-grey w3-padding-32 w3-padding-large" id="contact">
+    <div class="w3-content" style="max-width:600px">
+      <h4 class="w3-center"><b>Contact Me</b></h4>
+      <i class="material-icons">place</i>
+      <p>4 NO Road,D.I.T. Project,Dhaka 1212</p>
+      <i class="material-icons">mail</i>
+      <p>mamtazmaliha@gmail.com</p>
+      <i class="material-icons">local_phone</i>
+      <p>+8801797553830</p>
+    </div>
+  </div>
+
+    </div>
+  </footer>
+  
+  <div class="w3-black w3-center w3-padding-24">Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" 
+   <i class="material-icons">copyright</i><a>mamtazmaliha</a></div>
+
+<!-- End page content -->
+</div>
+
+<script>
+// Script to open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+</script>
+
+
+</body>
+</html>
+
